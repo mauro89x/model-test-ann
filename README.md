b2662ea2418d2d60f62b5bffde3c42c35d535de3
mauro89x <mauro89xdev@gmail.com>
2022-11-04 12:18:57 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Tópicos 2 App
+
+Simple app to test an ann model
+
+## Steps to run app
+
+1. Clone repository
+2. cd ~/model-test-ann
+3. npm install
+4. npm start
---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(port);
+console.log(`Server started at http://localhost:${port}`);
